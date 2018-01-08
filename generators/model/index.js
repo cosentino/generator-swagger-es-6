63108fc1fcaff27867bd2f71f881a48c72fb8075
Marcello Cosentino <cosentino.m2@2mlab.com>
2018-01-08 23:56:22 +0100

    - added 'js-yaml' package as devDependencies - implemented swagger.yaml file load and parse to json - added yeoman prompt asking for swagger definition from a preselected list

--- generators/model/index.js
+++ generators/model/index.js
@@ -2,6 +2,8 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 module.exports = class extends Generator {
   prompting() {
@@ -9,11 +11,41 @@ module.exports = class extends Generator {
       'Welcome to the primo model ' + chalk.red('generator-swagger-es-6') + ' generator!'
     ));
 
+    // Load the swagger.yaml file, convert it to json and look for the definition section
+    var swaggerDefinitions = [];
+    var swaggerDefinitionsNames = [];
+    try {
+      const ymlFile = fs.readFileSync('src/api/swagger/swagger.yaml', 'utf8');
+      const swaggerDoc = yaml.safeLoad(ymlFile);      
+      swaggerDefinitions = swaggerDoc.definitions;
+      for(var key in swaggerDefinitions) {
+        if(swaggerDefinitions.hasOwnProperty(key)) {
+          swaggerDefinitionsNames.push(key);
+        }
+      }      
+    } catch (e) {      
+      console.error('An error occurred parsing the swagger.yaml file', e);
+    }
+
     const prompts = [{
       type: 'input',
       name: 'name',
       message: 'Your new model name!'
     }];
+
+    if (swaggerDefinitionsNames && swaggerDefinitionsNames.length > 0) {
+      (function() {
+        prompts.push({
+          type: 'list',
+          name: 'definition',
+          message: 'The swagger definition you want to generate the mongoose schema from? ',
+          choices: swaggerDefinitionsNames,
+          default: function(userAnswers) {
+            return swaggerDefinitionsNames.indexOf(userAnswers.name);
+          }
+        });
+      })();
+    }
 
     return this.prompt(prompts).then(props => {
       this.props = props;
